7a8d97c417bad3db5afa1ccc30e3b0a82de24f41
lowgiant <lowgiant@gmail.com>
2021-07-25 14:06:51 +0900

    feat: Add 210725-10-2-ArrayString.c

---
+++ c/210725-10-2-ArrayString.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(void){
+    char *strings = "Hello world";
+    printf("%c\n",strings[1]);
+    printf("%c\n",strings[2]);
+    printf("%c\n",strings[3]);
+    return 0;
+}
