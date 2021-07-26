3cc740524c53f9898b98c3334f27a4c92e095c15
lowgiant <lowgiant@gmail.com>
2021-07-26 14:26:19 +0900

    feat: Add 210726-15-3-typedef.c

---
+++ c/210726-15-3-typedef.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+typedef struct Student{
+    char studentid[10];
+    char name[10];
+    int grade;
+    char major[51];
+} Student;
+
+int main(void){
+    Student s = {"20101111","lowgiant",4, "컴퓨터과학과"};
+    printf("학번: %s\n", s.studentid);
+    printf("이름: %s\n", s.name);
+    printf("학년: %d\n", s.grade);
+    printf("전공: %s\n", s.major);
+    
+    return 0;
+}
