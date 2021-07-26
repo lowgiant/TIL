a155a9e58c5c2b16fe00595488ba1ab54d8cb8be
lowgiant <lowgiant@gmail.com>
2021-07-26 15:21:11 +0900

    feat: Add 210726-16-1-filewriter.c

---
+++ c/210726-16-1-filewriter.c
@@ -0,0 +1,12 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main(void){
+    char s[20] = "Hello world";
+    FILE *fp;
+    fp = fopen("temp.txt", "w");
+    fprintf(fp, "%s\n", s);
+    fclose(fp);
+
+    return 0; 
+}
