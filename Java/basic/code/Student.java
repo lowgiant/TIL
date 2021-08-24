dd7a7b25864327c86e5e016bc855058e385d1004
lowgiant <lowgiant@gmail.com>
2021-08-24 17:30:21 +0900

    feat: Add Student constructor

--- Java/basic/code/Student.java
+++ Java/basic/code/Student.java
@@ -2,7 +2,16 @@ public class Student {
     public String studentName;
     public String address;
 
+    public  Student(){
+    }
+
+    public Student(String studentName, String address){
+        this.studentName = studentName;
+        this.address = address;
+    }
+
     public void showStudentInfo(){
         System.out.println(studentName+", " + address);
     }
+
 }
